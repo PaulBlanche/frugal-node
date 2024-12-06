6e994c3c4b9bb7a33463dccf725ec1debc29d8ff
Raphaël Ligier <github@whiteshoulders.fr>
2024-12-06 22:59:01 +0100

    cache control headers

--- doc/src/pages/home/page.ts
+++ doc/src/pages/home/page.ts
@@ -5,7 +5,7 @@ import { Page } from "./Page.tsx";
 export const route = "/";
 
 export function build() {
-	return PageResponse.data({ foo: "bar" }, { maxAge: 5 });
+	return PageResponse.data({ foo: "bar" }, { maxAge: 60 });
 }
 
 export const render = getRenderFrom(Page);
